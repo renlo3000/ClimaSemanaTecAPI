cb84f679fc98c821c1e47195eb86d9265c41fde3
Oscar Diaz <oscar01mx@gmail.com>
2025-09-15 11:53:22 -0600

    Preparing repo

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -10,31 +10,4 @@ public class HelloController {
     public String hello() {
         return "Hello, World!";
     }
-    
-    @GetMapping
-    public String getAllItems() {
-        return "Returning all items (GET)";
-    }
-
-    @GetMapping("/{id}")
-    public String getItemById(@PathVariable String id) {
-        return "Returning item with id: " + id + " (GET)";
-    }
-
-    @PostMapping
-    public String createItem() {
-        return "Creating a new item (POST)";
-    }
-
-    @PutMapping("/{id}")
-    public String updateItem(@PathVariable String id) {
-        return "Updating item with id: " + id + " (PUT)";
-    }
-
-    @DeleteMapping("/{id}")
-    public String deleteItem(@PathVariable String id) {
-        return "Deleting item with id: " + id + " (DELETE)";
-    }
-
-
 }
