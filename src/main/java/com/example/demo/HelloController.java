683b52a8a28c6747a2cebfdb4c6c0be7c9bdb843
Oscar Diaz <oscar01mx@gmail.com>
2025-09-16 06:56:27 -0600

    Agregando ejercicio en clase

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,21 @@ import org.springframework.web.bind.annotation.*;
 public class HelloController {
 
     @GetMapping("/get")
-    public String hello() {
+    public String getHello() {
         return "Hello, World!";
+    }
+
+    @PostMapping
+    public String postHello() {
+        return "Created item";
+    }
+    @PutMapping("/{id}")
+    public String putHello(@PathVariable String id) {
+        return "Updated item with id: " + id;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteHello(@PathVariable String id) {   
+        return "Deleted item with id: " + id;
     }
 }
